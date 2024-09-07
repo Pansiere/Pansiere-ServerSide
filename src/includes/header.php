69d571a09726411f91d30c1a3d70024159b1fb22
João Pedro V. Pansiere <ojp027@gmail.com>
2024-09-07 20:44:49 -0300

    update

--- src/includes/header.php
+++ src/includes/header.php
@@ -5,21 +5,21 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="styles/home.css" />
-    <title>Projeto</title>
+    <link rel="stylesheet" href="assets/styles/home.css" />
+    <title>Side Server</title>
 </head>
 
 <body>
     <div class="header">
+        <div class="banner">
+            <h1>Pansiere</h1>
+        </div>
         <div class="meuGitHub">
             <a href="https://github.com/seuperfil" target="_blank">GitHub</a>
         </div>
-        <div class="banner">
-            <img src="assets/banner.jpg">
-        </div>
         <nav>
-            <a href="#sobre">Sobre Mim</a> |
-            <a href="#projetos">Projetos</a> |
+            <a href="#sobre">Sobre Mim</a>
+            <a href="#projetos">Projetos</a>
             <a href="#contato">Contato</a>
         </nav>
     </div>
